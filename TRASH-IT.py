9b2ba31d3385896347d82c319bad5f561bc12a2d
jmaa0003 <119824296+jmaa0003@users.noreply.github.com>
2023-11-12 21:15:15 +0100

    Update TRASH-IT.py
    """
    j'ai crée la fonction isMultiple pour la raison suivante:
    
    Déjà, nous savons qu'un carré du sudoku est de taille 3x3, et que donc les indices seront de 0 à 2 compris.
    je veux effectuer mon opération de choix des candidats uniques sur chacuns des sous_carrés du sudoku.
    Alors pour le premier carré, je n'itererai que de 0,0 à 2,2, effectivement. Et cela peut facilement être fait avec un range ou une borne.
    
    Cependant si je veux aller au carré suivant, je dois garder ce même bornage (de 0+(i,j) à 2+(i,j)).; Comment faire ?
    Le problème du modulo ici est qu'il me renverra True pour i % l_carre == 0 mais le souci est que si i vaut 0, cela donne le même résultat, alors que je ne suis pas à la fin de mon carré.
    
    Pour y remedier, On pouvait faire i % l_carre (2) == 2 mais j'ai décidé de créer une fonction pour flex en regardant si i est un multiple de l_carre que j'ai mis à trois chez moi.
    """

--- TRASH-IT.py
+++ TRASH-IT.py
@@ -6,21 +6,57 @@ une matrice 9 x 9 d’entiers représentant une grille de sudoku,
 et qui renvoie un couple de valeurs.
 """
 #3 étapes pour sudoku:
-# 1. Itérer jusqu'à trouver des 0
+# 0. Hallar las 0s
+# 1. Vérifier les carrées
+# 2. Vérifier les lignes et les colonnes
+
 
 def naked_single(grid):
-    i,i_max,j,j_max, chiffres  = 0,len(grid),0,len(grid[-1]), [i for i in range(1,10)]
----   #I thought about deleting this section and doing it in a miscellaneous way 
-    while i < i_max:
-        colonne, ligne = grid[j][i], grid[i]
-        
-        
-        if j < i_max-1:
-            j += 1
+    IJ_MAX, CHIFFRES, CARRE_TAILLE =\
+                 len(grid), set([i for i in range(1,10)]), 3
+    carre, i, j, ok = {}, 0, 0, False
+    
+    def isMultiple(M,b):
+        if b == M or M == 1:
+            res = True
+        elif b < M:
+            res = False
         else:
-            j = 0
-            i += 1
----       
+            res = isMultiple(M,b-M)
+        
+        return res
+    while i < IJ_MAX and not ok: 
+        #pour chaque carre le bloc ci dessous:
+        while not isMultiple(CARRE_TAILLE, i):
+            carre[(i,j)] = grid[i][j]
+            if not isMultiple(CARRE_TAILLE-1, j):
+                j += 1
+            else:
+                j = 0
+                i += 1
+
+        for _ in carre:
+            if carre[_] == 0:
+                #vérification ligne colonne carre commence !
+                candidats_uniques = CHIFFRES.difference(set(carre.values())) #0 aussi
+                x,y = _[0], _[1]
+                ligne = [li for li in grid[x]]
+                colonne, col = [], 0
+                while col < IJ_MAX:
+                    colonne.append(grid[col][y])
+                    col += 1
+                candidats_uniques -= set(colonne).union(set(ligne))
+                # élimination de tous les chiffres présents dans la ligne, colonne et carre en même temps
+                # pour obtenir un ou des candidats uniques
+                
+                if len(candidats_uniques) > 0:
+                    grid[x][y] = carre[_] = candidats_uniques.pop() #prendre un candidat au hasard MAIS l'enlever du set
+                else:
+                    ok = True
     
-    
-    
+    if not ok == True:
+        res = (not ok, grid)
+    else:
+        res = (not ok, None)
+        
+    return res #renvoie True si 'ok' est resté à False donc aucun carré n'a causé un arrêt de la résolution du sudoku. 
